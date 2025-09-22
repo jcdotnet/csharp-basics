2ba4c4bf45cf7c1c8edd816d05866684fb11370e
JC <jcdotnet@hotmail.com>
2025-09-22 14:19:44 +0200

    Added C# types documentation

---
+++ JC/ReferenceTypes/Program.cs
@@ -0,0 +1,4 @@
+﻿// Reference types
+// https://learn.microsoft.com/en-us/dotnet/csharp/language-reference/keywords/reference-types
+// built-in types
+// https://learn.microsoft.com/en-us/dotnet/csharp/language-reference/builtin-types/reference-types
