a8f53d4c27cf039255bbe57e853f979cd48185ed
JC <jcdotnet@hotmail.com>
2025-10-02 19:38:10 +0200

    feat :: asp.net core MVC demo project

---
+++ asp.net/core/MvcDemo/MvcDemo/Controllers/ProductController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MvcDemo.Controllers
+{
+    public class ProductController : Controller
+    {
+        [Route("products")]
+        public IActionResult Index()
+        {
+            return View(); // first Views/Product/Index.cshtml, then Views/Shared/Index.cshtml
+        }
+    }
+}
---
+++ asp.net/core/MvcDemo/MvcDemo/Models/Person.cs
@@ -0,0 +1,17 @@
+﻿namespace MvcDemo.Models
+{
+    public class Person
+    {
+        public int? Id { get; set; }
+        public string? Name { get; set; }
+        public Gender Gender { get; set; }
+        public DateTime? BirthDate { get; set; }
+
+    }
+
+    public enum Gender
+    {
+        Male, Female, Other
+
+    }
+}
