9e0429f359959f079fb70638dc18fda2e31f4606
JC <jcdotnet@hotmail.com>
2025-10-03 17:01:50 +0200

    refactor(MVC demo project) :: customers list

--- asp.net/core/MvcDemo/MvcDemo/Controllers/CustomerController.cs
+++ asp.net/core/MvcDemo/MvcDemo/Controllers/CustomerController.cs
@@ -5,14 +5,15 @@ namespace MvcDemo.Controllers
 {
     public class CustomerController : Controller
     {
+        private readonly List<Customer> customers = [
+            new Customer() { Id = 1, Name = "John Doe", Gender = Gender.Male, BirthDate = Convert.ToDateTime("1980/06/03") },
+            new Customer() { Id = 2, Name = "Mary Doe", Gender = Gender.Male, BirthDate = Convert.ToDateTime("1980/06/03") },
+            new Customer() { Id = 3, Name = "Jane Doe", Gender = Gender.Male},
+        ];
+
         [Route("/customers")]
         public IActionResult Index()
         {
-            List<Customer> customers = [
-                new Customer() { Name = "John Doe", Gender = Gender.Male, BirthDate = Convert.ToDateTime("1980/06/03") },
-                new Customer() { Name = "Mary Doe", Gender = Gender.Male, BirthDate = Convert.ToDateTime("1980/06/03") },
-                new Customer() { Name = "Jane Doe", Gender = Gender.Male},
-            ];
             ViewData["customers"] = customers;
             // return new ViewResult() { ViewName="Index"}; // implements IActionResult
             return View();                                  // simplified, real world projects version  
@@ -25,11 +26,6 @@ namespace MvcDemo.Controllers
             if (id is null) return BadRequest("Id cannot be empty");
             if (id > 3 ) return NotFound("Person Not Found");
 
-            List<Customer> customers = [
-                new Customer() { Id = 1, Name = "John Doe", Gender = Gender.Male, BirthDate = Convert.ToDateTime("1980/06/03") },
-                new Customer() { Id = 2, Name = "Mary Doe", Gender = Gender.Male, BirthDate = Convert.ToDateTime("1980/06/03") },
-                new Customer() { Id = 3, Name = "Jane Doe", Gender = Gender.Male},
-            ];
             Customer? customer = customers.Where(p => p.Id == id).FirstOrDefault();
             return View(customer);                          // using a strongly typed view instead of ViewData
         }
