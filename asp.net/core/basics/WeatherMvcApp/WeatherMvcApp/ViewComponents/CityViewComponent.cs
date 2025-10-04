df6b5de43c979f83e54d92ba4e1974253dd1e03c
JC <jcdotnet@hotmail.com>
2025-10-04 14:00:01 +0200

    feat(weather app) :: views components

---
+++ asp.net/core/basics/WeatherMvcApp/WeatherMvcApp/ViewComponents/CityViewComponent.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WeatherMvcApp.Models;
+
+namespace WeatherMvcApp.ViewComponents
+{
+    public class CityViewComponent : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync(CityWeather city)
+        {
+            ViewBag.CityCssClass = GetCssClass(city.Temperature);
+            return View(city);
+        }
+
+        private string GetCssClass(int temperature)
+        {
+            return temperature switch
+            {
+                (< 44) => "blue-back",
+                (>= 44) and (< 75) => "green-back",
+                (>= 75) => "orange-back"
+            };
+        }
+    }
+}
