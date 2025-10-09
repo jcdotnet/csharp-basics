3125043c3fcd795b5aafe4a492bfaec5b0e98fe8
JC <jcdotnet@hotmail.com>
2025-10-09 11:59:37 +0200

    test(ContactsManager, CountriesService) :: add country

---
+++ asp.net/core/ContactsManager/ContactsManager.Tests/CountriesServiceTest.cs
@@ -0,0 +1,77 @@
+﻿using ServiceContracts;
+using ServiceContracts.DTO;
+using Services;
+
+namespace ContactsManager.Tests
+{
+    public class CountriesServiceTest
+    {
+        private readonly ICountriesService _service;
+
+        public CountriesServiceTest()
+        {
+            _service = new CountriesService();
+        }
+
+        // Requirement: when country is null, it should throw ArgumentNullException
+        [Fact]
+        public void AddCountry_NullCountry()
+        {
+            // Arrange
+            CountryAddRequest? request = null;
+
+            // Assert
+            Assert.Throws<ArgumentNullException>(() =>
+            {
+                // Act
+                _service.AddCountry(request);
+            });
+        }
+
+        // Requirement: when country name is null, it should throw ArgumentException
+        [Fact]
+        public void AddCountry_NullCountryName()
+        {
+            // Arrange
+            CountryAddRequest? request = new CountryAddRequest() { Name = null };
+
+            // Assert
+            Assert.Throws<ArgumentException>(() =>
+            {
+                // Act
+                _service.AddCountry(request);
+            });
+        }
+
+        // Requirement: when country name is duplicate, it should throw ArgumentException
+        [Fact]
+        public void AddCountry_DuplicateCountryName()
+        {
+            // Arrange
+            CountryAddRequest? request1 = new CountryAddRequest() { Name = "Spain" };
+            CountryAddRequest? request2 = new CountryAddRequest() { Name = "Spain" };
+
+            // Assert
+            Assert.Throws<ArgumentException>(() =>
+            {
+                // Act
+                _service.AddCountry(request1);
+                _service.AddCountry(request2);
+            });
+        }
+
+        // Requirement: when proper country name, it should insert it in the list of countries
+        [Fact]
+        public void AddCountry()
+        {
+            // Arrange
+            CountryAddRequest? request = new CountryAddRequest() { Name = "Spain" };
+
+            // Act
+            CountryResponse? response = _service.AddCountry(request);
+
+            // Assert
+            Assert.True(response.Id != Guid.Empty);
+        }
+    }
+}
---
+++ asp.net/core/ContactsManager/Services/CountriesService.cs
@@ -0,0 +1,41 @@
+﻿using Entities;
+using ServiceContracts;
+using ServiceContracts.DTO;
+
+namespace Services
+{
+    public class CountriesService : ICountriesService
+    {
+        private List<Country> _countries;
+
+        public CountriesService() { 
+            _countries = new List<Country>();
+        }
+
+        public CountryResponse AddCountry(CountryAddRequest? countryAddRequest)
+        {
+            // validations
+            if (countryAddRequest == null)
+            {
+                throw new ArgumentNullException(nameof(countryAddRequest));
+            }
+
+            if (countryAddRequest.Name == null)
+            {
+                throw new ArgumentException(nameof(countryAddRequest.Name));
+            }
+
+            if (_countries.Where(c => c.Name == countryAddRequest.Name).Count() > 0)
+            {
+                throw new ArgumentException("Country already exists");
+            }
+            // end validations
+
+            Country country = countryAddRequest.ToCountry();
+            country.Id = Guid.NewGuid();
+            _countries.Add(country);
+
+            return country.ToCountryResponse();
+        }
+    }
+}
