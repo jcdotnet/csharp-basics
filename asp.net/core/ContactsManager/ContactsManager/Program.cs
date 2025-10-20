d95644eef2ab69415b4ddfe50aec9f06594af054
JC <jcdotnet@hotmail.com>
2025-10-20 16:48:13 +0200

    refactor(ContactsManager) :: services

--- asp.net/core/ContactsManager/ContactsManager/Program.cs
+++ asp.net/core/ContactsManager/ContactsManager/Program.cs
@@ -1,24 +1,8 @@
-using Entities;
-using Microsoft.EntityFrameworkCore;
-using Repositories;
-using RepositoryContracts;
-using ServiceContracts;
-using Services;
+using ContactsManager.StartupExtensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// IoC container
-builder.Services.AddControllersWithViews();
-builder.Services.AddScoped<ICountriesRepository, CountriesRepository>();
-builder.Services.AddScoped<IContactsRepository, ContactsRepository>();
-builder.Services.AddScoped<ICountriesService, CountriesService>();
-builder.Services.AddScoped<IContactsService, ContactsService>();
-
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
-{
-    //options.UseSqlServer(builder.Configuration["ConnectionStrings::Default"]);
-    options.UseSqlServer(builder.Configuration.GetConnectionString("Default"));
-});
+builder.Services.ConfigureServices(builder.Configuration);
 
 var app = builder.Build();
 
---
+++ asp.net/core/ContactsManager/ContactsManager/StartupExtensions/ConfigureServicesExtension.cs
@@ -0,0 +1,31 @@
+﻿using Entities;
+using Microsoft.EntityFrameworkCore;
+using Repositories;
+using RepositoryContracts;
+using ServiceContracts;
+using Services;
+
+namespace ContactsManager.StartupExtensions
+{
+    public static class ConfigureServicesExtension
+    {
+        public static IServiceCollection ConfigureServices(this IServiceCollection services,
+            IConfiguration configuration)
+        {
+            // IoC container
+            services.AddControllersWithViews();
+            services.AddScoped<ICountriesRepository, CountriesRepository>();
+            services.AddScoped<IContactsRepository, ContactsRepository>();
+            services.AddScoped<ICountriesService, CountriesService>();
+            services.AddScoped<IContactsService, ContactsService>();
+
+            services.AddDbContext<ApplicationDbContext>(options =>
+            {
+                //options.UseSqlServer(configuration["ConnectionStrings::Default"]);
+                options.UseSqlServer(configuration.GetConnectionString("Default"));
+            });
+
+            return services;
+        }
+    }
+}
