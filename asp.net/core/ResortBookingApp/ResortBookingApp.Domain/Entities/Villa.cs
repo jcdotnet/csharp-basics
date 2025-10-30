1091d992b44bb5b6fbec5ecc74d305d732eb0fbf
JC <jcdotnet@hotmail.com>
2025-10-30 15:32:08 +0100

    feat :: Resort Booking MVC demo app

---
+++ asp.net/core/ResortBookingApp/ResortBookingApp.Domain/Entities/Villa.cs
@@ -0,0 +1,16 @@
+﻿namespace ResortBookingApp.Domain.Entities
+{
+    public class Villa
+    {
+        public  int Id { get; set; }
+        public required string Name { get; set; }
+        public string? Description { get; set; }
+        public double Price { get; set; }
+        public int SquareMeters { get; set; }
+        public int Occupancy { get; set; }
+        public string? ImageUrl { get; set; }
+        public DateTime? CreatedDate { get; set; }
+        public DateTime? UpdatedDate { get; set; }
+
+    }
+}
