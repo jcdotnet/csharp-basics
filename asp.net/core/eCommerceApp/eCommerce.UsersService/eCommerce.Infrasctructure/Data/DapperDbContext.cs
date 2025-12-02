06801e5a469308f62a8fb3a0b9b4a35a34b7a057
JC <jcdotnet@hotmail.com>
2025-12-02 18:04:32 +0100

    feat(eCommerceApp, users microservice) :: updated connection string

--- asp.net/core/eCommerceApp/eCommerce.UsersService/eCommerce.Infrasctructure/Data/DapperDbContext.cs
+++ asp.net/core/eCommerceApp/eCommerce.UsersService/eCommerce.Infrasctructure/Data/DapperDbContext.cs
@@ -11,7 +11,13 @@ namespace eCommerce.Infrastructure.Data
         public DapperDbContext(IConfiguration configuration)
         {
             _configuration = configuration;
-            string? connectionString = _configuration.GetConnectionString("PostgresConnection");
+            string connectionTemplate = _configuration.GetConnectionString("PostgresConnection")!;
+            string connectionString = connectionTemplate
+                .Replace("$POSTGRES_HOST", Environment.GetEnvironmentVariable("POSTGRES_HOST"))
+                .Replace("$POSTGRES_PORT", Environment.GetEnvironmentVariable("POSTGRES_PORT"))
+                .Replace("$POSTGRES_DATABASE", Environment.GetEnvironmentVariable("POSTGRES_DATABASE"))
+                .Replace("$POSTGRES_USERNAME", Environment.GetEnvironmentVariable("POSTGRES_USERNAME"))
+                .Replace("$POSTGRES_PASSWORD", Environment.GetEnvironmentVariable("POSTGRES_PASSWORD"));
             _connection = new NpgsqlConnection(connectionString);   
         }
 
