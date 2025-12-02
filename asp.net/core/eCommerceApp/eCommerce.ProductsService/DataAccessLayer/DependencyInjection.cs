90eebc05ff8813352eec6c4583031945535405fc
JC <jcdotnet@hotmail.com>
2025-12-02 18:46:38 +0100

    feat(eCommerceApp, products microservice) :: updated connection string

--- asp.net/core/eCommerceApp/eCommerce.ProductsService/DataAccessLayer/DependencyInjection.cs
+++ asp.net/core/eCommerceApp/eCommerce.ProductsService/DataAccessLayer/DependencyInjection.cs
@@ -13,9 +13,16 @@ namespace DataAccessLayer
             this IServiceCollection services,
             IConfiguration configuration)
         {
+            var connectionStringTemplate = configuration.GetConnectionString("DefaultConnection")!;
+            var connectionString = connectionStringTemplate
+                .Replace("$MYSQL_HOST", Environment.GetEnvironmentVariable("MYSQL_HOST"))
+                .Replace("$MYSQL_PORT", Environment.GetEnvironmentVariable("MYSQL_PORT"))
+                .Replace("$MYSQL_DATABASE", Environment.GetEnvironmentVariable("MYSQL_DATABASE"))
+                .Replace("$MYSQL_USERNAME", Environment.GetEnvironmentVariable("MYSQL_USERNAME"))
+                .Replace("$MYSQL_PASSWORD", Environment.GetEnvironmentVariable("MYSQL_PASSWORD") ?? "");
             services.AddDbContext<ApplicationDbContext>(options =>
             {
-                options.UseMySQL(configuration.GetConnectionString("DefaultConnection")!);
+                options.UseMySQL(connectionString);
             });
             services.AddScoped<IProductsRepository, ProductsRepository>();
             return services;
